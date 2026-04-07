1de2b8aa8a441a8b536b4d66e55884f69296ae03
unnatrathod123 <vrstatus78@gmail.com>
2026-04-07 16:11:03 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,3 @@
-// import mysql from 'mysql2/promise';
-
-// export const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-// });
-
-
 import { Pool } from 'pg';
 
 export const pool = new Pool({
