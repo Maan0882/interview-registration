1c00d192d90275516bd9eae96a86a7e79424b4c1
unnatrathod123 <vrstatus78@gmail.com>
2026-04-07 16:08:25 +0530

    Update db.ts for postgresql

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,22 @@
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
 
-export const pool = mysql.createPool({
+// export const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+
+
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // Required for Render
+  }
 });
