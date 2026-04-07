3482e5d34f6374730c4a4cbb59359b8b2288898f
unnatrathod123 <vrstatus78@gmail.com>
2026-04-07 16:17:32 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,8 @@
-import { Pool } from 'pg';
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for Render
-  }
+import mysql from 'mysql2/promise';
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
