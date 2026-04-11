cecf4399040bce96cab395f1110cc6e39d859bd7
unnatrathod123 <vrstatus78@gmail.com>
2026-04-11 11:42:57 +0530

    register page link removed from div

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ export default function Home()
 {
 
   return(
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <RegisterPage/>
-    </div>
   );
 }
